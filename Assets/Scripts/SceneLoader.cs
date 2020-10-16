0945ada6a6e14d562fe3e44cece93a4916686b0e
Dominic Barbuto <d-barbuto@hvcc.edu>
2020-10-16 19:50:26 -0400

    Assignment 6 Submission
    Assignment 6 Submission

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+        Debug.Log("Loaded Start Menu");
+    }
+
+    public void LoadNextLevel()
+    {
+        //TODO find scene index and add 1 to it
+    }
+
+    public void LoadLevel01()
+    {
+        SceneManager.LoadScene("Level01");
+        Debug.Log("Loaded Level 01");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+        Debug.Log("Loaded Game Over");
+    }
+
+    public void QuitApplication()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
