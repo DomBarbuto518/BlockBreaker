a442bde2e93a1ac468e4af1636f549bde7e09938
Dominic Barbuto <d-barbuto@hvcc.edu>
2020-10-16 22:09:07 -0400

    Blocks Part 2 Assignment
    Blocks Part 2 Assignment

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Vector2 ballPos;
+    [SerializeField] float initialBallVelocity = 5f;
+    [SerializeField] bool isLaunched;               //Flag variable
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLaunched = false;
+        Debug.Log("Ball is not launched");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Track Balls Position
+        ballPos = transform.position;
+
+        //Launch Ball
+        LaunchBall(initialBallVelocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(gameObject.name + " collided with " + collision.gameObject.name);
+    }
+
+    /**
+     * Uses the flag variable isLaunched to ensure the ball can only be launched once. 
+     * On start, isLaunched is false. Once mouse is clicked, ball is launched and isLaunched = true.
+     **/
+    private void LaunchBall(float velocity)
+    {
+       if(!isLaunched & Input.GetMouseButtonDown(0))
+        {
+            isLaunched = true;
+            Debug.Log("Ball is launched");
+            //Obtain reference to this game objects RigidBody2D component
+            Rigidbody2D ballRigidBody2D = GetComponent<Rigidbody2D>();
+            //Set velocity of ball
+            ballRigidBody2D.velocity = new Vector2(0, velocity);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] Vector2 paddlePos;
+    [SerializeField] float mouseXPosInUnits;
+    [SerializeField] float paddleMinX = 1f;
+    [SerializeField] float paddleMaxX = 15f;
+    const int SCREEN_WIDTH_IN_UNITS = 16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddlePos = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWithMouse();
+    }
+
+    private void MoveWithMouse()
+    {
+        mouseXPosInUnits = Input.mousePosition.x / Screen.width * SCREEN_WIDTH_IN_UNITS;    //Get Mouse position
+        //Set paddle position vector to mouses position
+        paddlePos.x = Mathf.Clamp(mouseXPosInUnits, paddleMinX, paddleMaxX);                
+        //Set paddles position with our new vector
+        transform.position = paddlePos;
+    }
+}
